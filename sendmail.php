a98ce11301bfa33b7ac13333a4de9c42d1753fc8
National-Beach <118359466+National-Beach@users.noreply.github.com>
2022-11-21 12:16:06 -0600

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -21,7 +21,6 @@ if(isset($_POST['submit'])){
     $mail->Username = 'noreply@nationalbeach.com'; // Gmail address which you want to use as SMTP server
     $mail->Password = 'austin123456789!!'; // Gmail address Password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
-    $mail->Port = '587';
 
     $mail->setFrom('noreply@nationalbeach.com'); // Gmail address which you used as SMTP server
     $mail->addAddress('noreply@nationalbeach.com'); // Email address where you want to receive emails (you can use any of your gmail address including the gmail address which you used as SMTP server)
